b746c4163b20cd5f10fe5ae1f13a961f69d36ce3
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2021-10-18 16:38:05 -0700

    move everything out of main and into side class, remove context.patient

--- Projects/BrachyRingChecker/BrachyRingChecker.cs
+++ Projects/BrachyRingChecker/BrachyRingChecker.cs
@@ -10,7 +10,7 @@ using VMS.TPS.Common.Model.Types;
 using BrachyRingChecker.Services;
 
 // TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
-[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyVersion("1.0.0.10")]
 [assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: AssemblyInformationalVersion("1.0")]
 
@@ -28,7 +28,6 @@ namespace VMS.TPS
     [MethodImpl(MethodImplOptions.NoInlining)]
     public void Execute(ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
     {
-            Patient patient = context.Patient;
             BrachyPlanSetup brachy_plan = context.BrachyPlanSetup;
             IEnumerable<Catheter> catheters = brachy_plan.Catheters;
             CheckForRing ring_checker = new CheckForRing();
@@ -38,60 +37,5 @@ namespace VMS.TPS
             }
       // TODO : Add here the code that is called when the script is launched from Eclipse.
     }
-        public void check_for_ring(Catheter catheter)
-        {
-            bool angle_is_ring = is_ring_from_angle(catheter);
-            if (angle_is_ring)
-            {
-                bool error_free = true;
-                if (catheter.ApplicatorLength != 1320.0)
-                {
-                    System.Windows.MessageBox.Show($"Potential ring in channel {catheter.ChannelNumber}, {catheter}, but the applicator " +
-                        $"length was {catheter.ApplicatorLength / 10}cm and should be 132cm");
-                    error_free = false;
-                }
-                if (catheter.DeadSpaceLength > 0)
-                {
-                    System.Windows.MessageBox.Show($"Potential ring in channel {catheter.ChannelNumber}, {catheter}, but the dead space " +
-                        $" was {catheter.DeadSpaceLength / 10}cm and should be 0cm");
-                    error_free = false;
-                }
-                if (error_free)
-                {
-                    System.Windows.MessageBox.Show($"Potential ring in channel {catheter.ChannelNumber}, {catheter}." +
-                        $" Use distal correction!");
-                }
-            }
-        }
-        public bool is_ring_from_angle(Catheter catheter)
-        {
-            int counter = 0;
-            List<VVector> points = new List<VVector>();
-            foreach (VVector i in catheter.Shape)
-            {
-                counter += 1;
-                points.Add(i);
-            }
-            double angle = 0;
-            for (int i = 0; i < counter - 2; i ++)
-            {
-                VVector vector_1 = points[i + 1] - points[i];
-                double norm_1 = Math.Sqrt(Math.Pow(vector_1.x, 2) + Math.Pow(vector_1.y, 2) + Math.Pow(vector_1.z, 2));
-                vector_1 /= norm_1;
-
-                VVector vector_2 = points[i + 2] - points[i + 1];
-                double norm_2 = Math.Sqrt(Math.Pow(vector_2.x, 2) + Math.Pow(vector_2.y, 2) + Math.Pow(vector_2.z, 2));
-                vector_2 /= norm_2;
-
-                double dot_product = vector_2.x * vector_1.x + vector_2.y * vector_1.y + vector_2.z * vector_1.z;
-                double angle_degrees = Math.Acos(dot_product) * 180 / Math.PI;
-                angle += angle_degrees;
-            }
-            if (angle > 90) // If we have over 90 degrees, this is definitely a ring...
-            {
-                return true;
-            }
-            return false;
-        }
   }
 }
